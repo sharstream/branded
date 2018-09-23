022c9607555dc806cdf44c455aaa35b108e95b24
David Perez <david.perez@us.abb.com>
2018-09-23 17:48:06 -0400

    oauth with okta only-backend

--- server.js
+++ server.js
@@ -4,8 +4,20 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
+ require('dotenv').config({
+  silent: true
+});
 var express = require("express");
+const session = require("express-session");
+const ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
 var bodyParser = require("body-parser");
+
+// Globals
+const OKTA_ISSUER_URI = process.env.OKTA_ISSUER_URI;
+const OKTA_CLIENT_ID = process.env.OKTA_CLIENT_ID;
+const OKTA_CLIENT_SECRET = process.env.OKTA_CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const SECRET = process.env.SECRET;
 
 // Sets up the Express App
 // =============================================================
@@ -31,10 +43,38 @@ app.use(express.static("public"));
 // =============================================================
 require("./routes/api-movies.js")(app);
 
+app.use(session({
+  cookie: {httpOnly: true},
+  secret: SECRET
+}));
+
+//Authentication
+let oidc = new ExpressOIDC({
+  issuer: OKTA_ISSUER_URI,
+  client_id: OKTA_CLIENT_ID,
+  client_secret: OKTA_CLIENT_SECRET,
+  redirect_uri: REDIRECT_URI,
+  routes: {
+      callback: {
+          defaultRedirect: "/dashboard"
+      }
+  },
+  scope: "openid profile"
+});
+
+app.use(oidc.router);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+
+//Server management
+oidc.on("ready", () => {
+  db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+      console.log("App listening on PORT " + PORT);
+    });
   });
+});
+
+oidc.on("error", err => {
+  console.error(err);
 });
