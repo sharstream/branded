82b2fbc96695765972e350229cae235638e933b9
David Perez <kitsunebrave@gmail.com>
2018-12-09 17:44:17 -0500

    passport and es6 const global scope

--- server.js
+++ server.js
@@ -7,16 +7,17 @@
  require('dotenv').config({
   silent: true
 });
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require('passport');
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+const db = require("./models");
 
 // Sets up the Express app to handle data parsing
 
@@ -30,9 +31,15 @@ app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
 
+// Passport Middleware
+// =============================================================
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 // Routes
 // =============================================================
 require("./routes/api-movies.js")(app);
+require("./routes/api-users.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
