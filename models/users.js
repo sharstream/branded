1568996cd35b7546200cad5eb99cf9d80bd2702d
David Perez <kitsunebrave@gmail.com>
2018-12-09 17:44:58 -0500

    users.js schema

--- models/users.js
+++ models/users.js
@@ -1,26 +1,25 @@
 // Dependencies
 // =============================================================
 //src/models/users.js
-
 export default (sequelize, DataTypes) => 
   sequelize.define('User', {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      auteIncrement: true
+      autoIncrement: true,
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: true
+      allowNull: false
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: true
+      allowNull: false
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: true
+      allowNull: false
     },
     avatar: {
       type: DataTypes.STRING
